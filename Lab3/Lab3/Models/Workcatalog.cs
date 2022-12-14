9ec4c7f661b1b5902d12d676da1b9636ae5e1f0e
Barhan4ik <105607598+Barhan4ik@users.noreply.github.com>
2022-12-14 18:30:32 +0300

    Delete Lab3 directory

--- Lab3/Lab3/Models/Workcatalog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab3.Models
-{
-    public class Workcatalog
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int price { get; set; }
-    }
-}
